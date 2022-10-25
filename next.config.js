160b4bf58cdec85bd403dfc4acea839e02b44111
Ray Guo <ma2808203259@hotmail.com>
2022-10-25 17:22:42 +1100

    feat: github action to deploy on ghpage

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  basePath: process.env.NODE_ENV === 'production' ? '/nextjsDemo' : '',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId },
+  ) {
+    return {
+      '/': { page: '/' },
+    };
+  },
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
