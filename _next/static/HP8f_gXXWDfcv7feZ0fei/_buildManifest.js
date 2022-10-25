90d99e661850e79312ef25fb7e0878f3a7522fd1
RayGuo-ergou <RayGuo-ergou@users.noreply.github.com>
2022-10-25 07:14:36 +0000

    deploy: 5e5eb89843d63eaced7a7366cb0aa1b17db5a0cd

--- _next/static/HP8f_gXXWDfcv7feZ0fei/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-a77248bd227f94a8.js"],"/_error":["static/chunks/pages/_error-fa2da97f07e0d864.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
